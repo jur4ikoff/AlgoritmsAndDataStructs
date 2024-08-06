3eb61c67af5c36484675f4af4c39da7bd208b253
1 <abc@gmail.com>
2024-08-06 03:03:38 +0300

    rework merge sort to python style

--- vk_education_algs_and_structs/4_merge_sort.py
+++ vk_education_algs_and_structs/4_merge_sort.py
@@ -2,51 +2,39 @@ n = int(input())
 array = list(map(int, input().split()))
 
 
-def merge(array: int, left: int, mid: int, right: int):
-    # Нисходящая сортировка присваиваением
-    sub_array_one = mid - left + 1
-    sub_array_two = right - mid
+def merge(left, right):
+    sorted_array = []
+    i = j = 0
 
-    # create temp array
-    left_array = [0] * sub_array_one
-    right_array = [0] * sub_array_two
-
-    for i in range(sub_array_one):
-        left_array[i] = array[left + i]
-    for i in range(sub_array_two):
-        right_array[i] = array[mid + i + 1]
-
-    index_array_one, index_array_two, index_of_merged = 0, 0, left
-
-    while index_array_one < sub_array_one and index_array_two < sub_array_two:
-        if left_array[index_array_one] <= right_array[index_array_two]:
-            array[index_of_merged] = left_array[index_array_one]
-            index_array_one += 1
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_array.append(left[i])
+            i += 1
         else:
-            array[index_of_merged] = right_array[index_array_two]
-            index_array_two += 1
-        index_of_merged += 1
+            sorted_array.append(right[j])
+            j += 1
 
-    while index_array_one < sub_array_one:
-        array[index_of_merged] = left_array[index_array_one]
-        index_array_one += 1
-        index_of_merged += 1
+    while i < len(left):
+        sorted_array.append(left[i])
+        i += 1
 
-    while index_array_two < sub_array_two:
-        array[index_of_merged] = right_array[index_array_two]
-        index_array_two += 1
-        index_of_merged += 1
+    while j < len(right):
+        sorted_array.appened(right[i])
+        j += 1
 
-
-def merge_sort(array: int, begin: int, end: int):
-    if begin >= end:
-        return
-
-    mid = begin + (end - begin) // 2
-    merge_sort(array, begin, mid)
-    merge_sort(array, mid + 1, end)
-    merge(array, begin, mid, end)
+    return sorted_array
 
 
-merge_sort(array, 0, n - 1)
+def merge_sort(array: list):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left_half = merge_sort(array[:mid])
+    right_half = merge_sort(array[mid:])
+
+    return merge(left_half, right_half)
+
+
+array = merge_sort(array)
 print(*array)
