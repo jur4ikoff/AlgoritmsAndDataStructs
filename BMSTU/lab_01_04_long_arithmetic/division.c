fbd78e03e657ff8ee7907d26d2be1cf87d92f817
1 <ypopov2005@gmail.com>
2024-09-18 23:20:00 +0300

    fix bugs

--- BMSTU/lab_01_04_long_arithmetic/division.c
+++ BMSTU/lab_01_04_long_arithmetic/division.c
@@ -56,16 +56,17 @@ void copy_to_struct(long long value, number_t *dest)
 
 void find_part_divisible(number_t *part_divisible, number_t divisible, number_t divider, number_t *result)
 {
-    int i = 0; // rc = ERR_OK;
+    int i = 0;
     int size_mantise_to_copy = 0;
     if (divisible.mantise_size < divider.mantise_size)
     {
-        size_mantise_to_copy = divider.mantise_size; // TO DO
+        size_mantise_to_copy = divider.mantise_size;
         bool is_only_zero = true;
         for (size_t j = divisible.mantise_size; j < (size_t)divider.mantise_size; j++)
         {
             if (divider.mantise[j] != 0)
                 is_only_zero = false;
+            result->order--; // TO DO ПРОВЕРИТЬ
         }
         if (!is_only_zero)
             size_mantise_to_copy += 1;
@@ -88,42 +89,6 @@ void find_part_divisible(number_t *part_divisible, number_t divisible, number_t 
 
     part_divisible->mantise_size = size_mantise_to_copy;
     copy(divisible, part_divisible, 0, size_mantise_to_copy);
-
-    /*do
-    {
-        printf("%d %d", divisible.mantise[i], divider.mantise[i]);
-        if (divisible.mantise[i] > divider.mantise[i])
-            part_divisible->mantise_size = divider.mantise_size + i;
-        else if (divisible.mantise[i] < divider.mantise[i])
-            part_divisible->mantise_size = divider.mantise_size + i + 1;
-        else
-            i++;
-
-    } while (part_divisible->mantise_size == 0 && i < divisible.mantise_size && i < divider.mantise_size);
-
-    if (i == divider.mantise_size && divisible.mantise[i - 1], divider.mantise[i - 1])
-    */
-    // printf("part_divisible->mantise_size = %d\n", part_divisible->mantise_size);
-
-    /*if (i == divisible.mantise_size)
-        part_divisible->mantise_size = i;
-
-    if (part_divisible->mantise_size == 0)
-        part_divisible->mantise_size = divisible.mantise_size + 1;
-
-    if (divider.mantise_size > divisible.mantise_size)
-        part_divisible->mantise_size += divider.mantise_size - divisible.mantise_size;*/
-
-    // copy(divisible, part_divisible, 0, part_divisible->mantise_size);
-    /*if (part_divisible->mantise_size > divisible.mantise_size)
-    {
-        printf("SRABOTALO\n");
-        part_divisible->mantise_size++;
-        part_divisible->order++;
-        result->order -= (divisible.mantise_size - part_divisible->mantise_size);
-    }*/
-    (void)*result;
-    // return ERR_OK;
 }
 
 void calculate_sign(number_t divisible, number_t divider, number_t *result)
@@ -137,16 +102,20 @@ void calculate_sign(number_t divisible, number_t divider, number_t *result)
 
 int long_div(number_t divisible, number_t divider, number_t *result)
 {
+    // Вычисление нового порядка и знака
     result->order = divisible.order - divider.order;
     calculate_sign(divisible, divider, result);
     number_t part_divisible = {.sign = 1};
-    find_part_divisible(&part_divisible, divisible, divider, result);
-    print_number(part_divisible);
 
+    // Получаем неполное делимое
+    find_part_divisible(&part_divisible, divisible, divider, result);
     int last_index = part_divisible.mantise_size;
-    do
+
+    while (part_divisible.mantise[0] != 0 && result->mantise_size < MAX_MANTISE)
     {
         long long int t1, t2, t;
+        printf("PART_DIV  ");
+        print_number(part_divisible);
         copy_to_number(&t1, part_divisible, 0, part_divisible.order - divider.mantise_size + 1);
         copy_to_number(&t2, divider, 0, divider.order - divider.mantise_size + 1);
         t = t1 / t2;
@@ -155,21 +124,16 @@ int long_div(number_t divisible, number_t divider, number_t *result)
         copy_to_number(&mantise_divider, divider, 0, divider.mantise_size);
         copy_to_number(&mantise_part_divisible, part_divisible, 0, part_divisible.mantise_size);
         long long int mult = t * mantise_divider;
-        // number_t remains = {0};
-        if (mantise_part_divisible - mult > 0)
-        {
-            ;
-        }
-        else
+
+        if (mantise_part_divisible - mult <= 0)
         {
             while (mantise_part_divisible - mult < 0)
             {
                 t -= 1;
                 mult = t * mantise_divider;
             }
-
-            // printf("%lld\n", mantise_part_divisible - mult);
         }
+
         printf("mult = %lld, t = %lld, part_mant_div = %lld\n", mult, t, mantise_part_divisible);
         // printf("%lld\n", t);
         result->mantise[result->mantise_size] = t;
@@ -191,11 +155,9 @@ int long_div(number_t divisible, number_t divider, number_t *result)
             last_index++;
         }
 
-        print_number(part_divisible);
-        // break;
-        printf("result: ");
-        print_number(*result);
-    } while (part_divisible.mantise[0] != 0 && result->mantise_size < MAX_MANTISE);
+        // printf("RES  ");
+        // print_number(*result);
+    }
 
     return ERR_OK;
 }
