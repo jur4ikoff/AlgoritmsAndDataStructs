fe6f5ae5a9c1aae5f045c010f5184ce92d04ed19
1 <ypopov2005@gmail.com>
2024-10-06 21:13:48 +0300

    test

--- BMSTU/lab_02_03_processing_tables/src/table.c
+++ BMSTU/lab_02_03_processing_tables/src/table.c
@@ -11,7 +11,22 @@ void create_key_table(table_t *key_table, students_t *array, size_t count)
     }
 }
 
+// Сортировка вставками
 void sort_key_table(table_t *key_table, size_t count)
 {
-    for (size_t i = 0)
+    char key[MAX_SURNAME_LEN], temp[MAX_STRING_LEN];
+    for (size_t i = 1; i < count; i++)
+    {
+        strcpy(key_table[i], key);
+        int j = i - 1;
+        while (j >= 0 && strcmp(key_table[j], key) > 0)
+        {
+            if (srcmp(key_table[j], key) > 0)
+            {
+                j--;
+                strcpy(key_table[j + 1], key_table[j]);
+            }
+            strcpy(key, key_table[j + 1]);
+        }
+    }
 }
