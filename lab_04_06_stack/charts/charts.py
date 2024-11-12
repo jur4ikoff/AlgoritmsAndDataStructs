f574977b54bf029180e207f0402cf28df99a7964
1 <ypopov2005@gmail.com>
2024-11-12 10:12:13 +0300

    update style

--- lab_04_06_stack/charts/charts.py
+++ lab_04_06_stack/charts/charts.py
@@ -10,16 +10,26 @@ script_directory = os.path.dirname(script_path)
 data_directory = f"{script_directory}/../"
 
 
-df_memory = pd.read_csv(f"{data_directory}data/memory.csv", delimiter=';')
+df_memory = pd.read_csv(f"{data_directory}data/memory.csv", delimiter=";")
 print("Создание графика памяти")
 fig = plt.figure(figsize=(10, 7))
 ax = fig.add_subplot(111)
-ax.plot(df_memory['size'], df_memory['static'], c='red', label="Размер стека на основе массива")
-ax.plot(df_memory['size'], df_memory['list'], c='blue',label="Размер стека на основе списка")
+ax.plot(
+    df_memory["size"],
+    df_memory["static"],
+    c="red",
+    label="Размер стека на основе массива",
+)
+ax.plot(
+    df_memory["size"],
+    df_memory["list"],
+    c="blue",
+    label="Размер стека на основе списка",
+)
 
-ax.set_xlabel('Размер стека')
-ax.set_ylabel('Объем занимаемой памяти (байты)')
-ax.set_title('График зависимости памяти, занимаемой каждым методом, от размера')
+ax.set_xlabel("Размер стека")
+ax.set_ylabel("Объем занимаемой памяти (байты)")
+ax.set_title("График зависимости памяти, занимаемой каждым методом, от размера")
 
 plt.legend(loc="upper left")
 plt.grid(True)
@@ -27,17 +37,27 @@ plt.savefig(f"{data_directory}/charts/memory_chart_5.svg")
 plt.show()
 
 
-speed_memory = pd.read_csv(f"{data_directory}data/speed.csv", delimiter=';')
+speed_memory = pd.read_csv(f"{data_directory}data/speed.csv", delimiter=";")
 print("Создание графика скорости")
 fig = plt.figure(figsize=(10, 7))
 ax = fig.add_subplot(111)
 
-ax.plot(speed_memory['size'], speed_memory['static'], c='red', label="Скорость стека на основе массива")
-ax.plot(speed_memory['size'], speed_memory['list'], c='blue',label="Скорость стека на основе списка")
+ax.plot(
+    speed_memory["size"],
+    speed_memory["static"],
+    c="red",
+    label="Скорость стека на основе массива",
+)
+ax.plot(
+    speed_memory["size"],
+    speed_memory["list"],
+    c="blue",
+    label="Скорость стека на основе списка",
+)
 
-ax.set_xlabel('Размер стека')
-ax.set_ylabel('Скорость (наносекунды)')
-ax.set_title('График зависимости скорости работы каждыого метода от размера')
+ax.set_xlabel("Размер стека")
+ax.set_ylabel("Скорость (наносекунды)")
+ax.set_title("График зависимости скорости работы каждыого метода от размера")
 
 plt.legend(loc="upper left")
 plt.grid(True)
