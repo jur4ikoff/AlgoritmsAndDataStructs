caae1e18948ffa4d54a47f761ad43fe9007a108d
Linux <ypopov2005@gmail.com>
2024-11-22 23:57:34 +0300

    fatal error

--- lab_05_07_queue/src/list_queue.c
+++ lab_05_07_queue/src/list_queue.c
@@ -10,19 +10,39 @@ void list_queue_init(list_queue_t *queue)
 }
 
 void list_queue_allocation(list_queue_t *queue)
-{}
+{
+}
 
 void list_queue_print_char(const list_queue_t queue)
 {
-    (void)queue;
+    if (!queue.next)
+    {
+        printf("%sОчередь пустая%s\n", YELLOW, RESET);
+        return;
+    }
+
+    list_queue_t *current = queue;
+    while (current)
+    {
+        printf("%c ", queue.data.element);
+        current = current->next;
+    }
 }
+
 int list_queue_push(list_queue_t *queue, char element)
 {
+    (void)queue;
+    (void)element;
     return ERR_OK;
 }
+
 int list_queue_pop(list_queue_t *queue, char *element)
 {
+    (void)queue;
+    (void)element;
     return ERR_OK;
 }
+
 void list_queue_free(list_queue_t *queue)
-{}
+{
+}
