432d93c59ee89ff8eb0f8be28f2022cf25167f03
Linux <ypopov2005@gmail.com>
2024-11-25 16:44:14 +0300

    add rand float

--- lab_05_07_queue/src/simulation.c
+++ lab_05_07_queue/src/simulation.c
@@ -1,6 +1,9 @@
+#define _GNU_SOURCE
+
 #include "simulation.h"
-#include "list_queue.h"
 #include "arr_queue.h"
+#include "list_queue.h"
+#include <stdlib.h>
 
 #define MAX_EXP_COUNT 1000
 #define STEP_REPORT 100
@@ -11,8 +14,14 @@ int T1_UPPER = 6;
 int T2_LOWER = 0;
 int T2_UPPER = 1;
 
+static float random_float(float min, float max)
+{
+    return min + (max - min) * ((float)rand() / (float)RAND_MAX);
+}
+
 void run_simulation_list_queue(void)
 {
-    // Код для запуска моделирования системы на основе списка 
-    
+    // Код для запуска моделирования системы на основе списка
+    float a = random_float(1, 2);
+    printf("%f\n", a);
 }
