b3718b9e2543294bc576f47360de1a0e9015e283
Linux <ypopov2005@gmail.com>
2024-11-26 01:25:47 +0300

    Списываем симуляцию.с

--- lab_05_07_queue/inc/simulation.h
+++ lab_05_07_queue/inc/simulation.h
@@ -7,7 +7,7 @@ extern int T1_UPPER;
 extern int T2_LOWER;
 extern int T2_UPPER;
 
-extern int MAX_EXP_COUNT;
+extern int MAX_REQUEST_COUNT;
 
 void run_simulation_list_queue(char *logfile, float *exp_time);
 
