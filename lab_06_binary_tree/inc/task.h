a529a32525ad9d03504b9c7e821c0b172da924ec
1 <ypopov2005@gmail.com>
2024-12-17 12:04:58 +0300

    delete lab_06

--- lab_06_binary_tree/inc/task.h
+++
@@ -1,9 +0,0 @@
-#ifndef TASK_H__
-#define TASK_H__
-#include "binary_tree.h"
-
-void convert_string_to_tree(tree_t **tree, char *string);
-void tree_delete_repeat(tree_t **tree);
-void remove_duplicates(tree_t **node);
-void string_remove_duplicates(char *str);
-#endif
--- lab_06_binary_tree/inc/test.h
+++
@@ -1,8 +0,0 @@
-#ifndef TESTS_H__
-#define TESTS_H__
-#include "binary_tree.h"
-#include <stdio.h>
-
-void test_binary_tree(void);
-size_t calculate_tree_size(tree_t *root);
-#endif
