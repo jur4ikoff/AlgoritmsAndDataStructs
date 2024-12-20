8d090c5696408b0a7d7a04ae74011064c135ae43
1 <ypopov2005@gmail.com>
2024-12-20 09:17:43 +0300

    add

---
+++ lab_07_01_hash_table_avl_tree/inc/avl_tree.h
@@ -0,0 +1,7 @@
+#ifndef AVL_TREE_H__
+#define AVL_TREE_H__
+
+typedef struct _avl_tree_type_ avl_tree;
+
+
+#endif
--- lab_07_01_hash_table_avl_tree/src/main.c
+++ lab_07_01_hash_table_avl_tree/src/main.c
@@ -20,6 +20,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "avl_tree.h"
 
 int main(void)
 {
